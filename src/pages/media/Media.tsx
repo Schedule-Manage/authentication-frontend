4bb0132a77c1fd97fd2d2ec3a07ff328c695cbec
Ian Kamau <kian99564@gmail.com>
2023-10-21 02:58:56 +0300

    Music upload implemented

--- src/pages/media/Media.tsx
+++ src/pages/media/Media.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 
 export default function Media() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const submitMusic = (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("music", selectedFile);
+
+    fetch(`${import.meta.env.VITE_SERVER_URL}music/upload`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <Navbar />
-      <form>
-        <input type="file" />
+      music/upload
+      <form onSubmit={submitMusic}>
+        <input
+          type="file"
+          onChange={(e: any) => setSelectedFile(e.target.files[0])}
+        />
         <button type="submit">Upload</button>
       </form>
     </div>
