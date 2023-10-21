3f894cac84e65a38d63c067660db82c92266aa5c
Ian Kamau <kian99564@gmail.com>
2023-10-21 14:44:01 +0300

    all history fetch bug fixed

--- src/pages/media/Media.tsx
+++ src/pages/media/Media.tsx
@@ -1,16 +1,12 @@
-import { Tabs, rem } from "@mantine/core";
-import {
-  IconPhoto,
-  IconMessageCircle,
-  IconSettings,
-} from "@tabler/icons-react";
+import { Tabs } from "@mantine/core";
 import { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 
 export default function Media() {
-  const iconStyle = { width: rem(12), height: rem(12) };
-  const [selectedFile, setSelectedFile] = useState();
-  const [allMusic, setAllMusic] = useState();
+  const [picked, setPicked] = useState();
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [allHistory, setAllHistory] = useState<any[]>([]);
+  const [allMusic, setAllMusic] = useState<any[]>([]);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_SERVER_URL}music`)
@@ -18,17 +14,44 @@ export default function Media() {
       .then((data) => setAllMusic(data.data));
   }, [allMusic]);
 
+  // Adding songs and getting all of song inside history
+  useEffect(() => {
+    fetch(
+      `${import.meta.env.VITE_SERVER_URL}music/history/playlist/${picked}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          console.log(data.data);
+          setAllHistory(data.data);
+        }
+      });
+  }, [picked]);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_SERVER_URL}music/history/playlist`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          setAllHistory(data.data);
+        }
+      });
+  }, [picked, allHistory]);
+
   const submitMusic = (e: any) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("music", selectedFile);
-
-    fetch(`${import.meta.env.VITE_SERVER_URL}music/upload`, {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+    if (selectedFile) {
+      formData.append("music", selectedFile);
+      fetch(`${import.meta.env.VITE_SERVER_URL}music/upload`, {
+        method: "POST",
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    }
   };
   return (
     <div>
@@ -36,30 +59,16 @@ export default function Media() {
 
       <Tabs defaultValue="gallery">
         <Tabs.List>
-          <Tabs.Tab
-            value="Add Music"
-            leftSection={<IconPhoto style={iconStyle} />}
-          >
-            Add Music
-          </Tabs.Tab>
-          <Tabs.Tab
-            value="All Music"
-            leftSection={<IconMessageCircle style={iconStyle} />}
-          >
-            All Music
-          </Tabs.Tab>
-          <Tabs.Tab
-            value="History"
-            leftSection={<IconSettings style={iconStyle} />}
-          >
-            History
-          </Tabs.Tab>
+          <Tabs.Tab value="Add Music">Add Music</Tabs.Tab>
+          <Tabs.Tab value="All Music">All Music</Tabs.Tab>
+          <Tabs.Tab value="History">History</Tabs.Tab>
         </Tabs.List>
 
         <Tabs.Panel value="Add Music">
           <form onSubmit={submitMusic}>
             <input
               type="file"
+              accept="audio/mp3"
               onChange={(e: any) => setSelectedFile(e.target.files[0])}
             />
             <button type="submit">Upload</button>
@@ -71,7 +80,12 @@ export default function Media() {
             <p>All Music</p>
             {allMusic &&
               allMusic.map((music: any) => (
-                <div key={music._id} onClick={() => console.log(music._id)}>
+                <div
+                  key={music._id}
+                  onClick={() => {
+                    setPicked(music._id);
+                  }}
+                >
                   <h1>{music.title}</h1>
                   {music.mimetype === "audio/mpeg" ? (
                     <audio controls>
@@ -87,7 +101,22 @@ export default function Media() {
           </div>
         </Tabs.Panel>
 
-        <Tabs.Panel value="History">Settings tab content</Tabs.Panel>
+        {allHistory && (
+          <Tabs.Panel value="History">
+            {allHistory && Array.isArray(allHistory) ? (
+              allHistory.map((music: any) => (
+                <div key={music._id}>
+                  <h1>{music.title}</h1>
+                  <audio controls>
+                    <source src={music.audio_url} type="audio/mpeg" />
+                  </audio>
+                </div>
+              ))
+            ) : (
+              <p>Loading or no data available.</p>
+            )}
+          </Tabs.Panel>
+        )}
       </Tabs>
 
       <div></div>
