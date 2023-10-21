d199ab36c5051079d56da7cef03959714da0f37d
Ian Kamau <kian99564@gmail.com>
2023-10-21 03:16:10 +0300

    Playing songs on the web

--- src/pages/media/Media.tsx
+++ src/pages/media/Media.tsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 
 export default function Media() {
   const [selectedFile, setSelectedFile] = useState();
+  const [allMusic, setAllMusic] = useState();
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_SERVER_URL}music`)
+      .then((res) => res.json())
+      .then((data) => setAllMusic(data.data));
+  }, []);
 
   const submitMusic = (e: any) => {
     e.preventDefault();
@@ -19,14 +26,33 @@ export default function Media() {
   return (
     <div>
       <Navbar />
-      music/upload
-      <form onSubmit={submitMusic}>
-        <input
-          type="file"
-          onChange={(e: any) => setSelectedFile(e.target.files[0])}
-        />
-        <button type="submit">Upload</button>
-      </form>
+
+      <div>
+        <form onSubmit={submitMusic}>
+          <input
+            type="file"
+            onChange={(e: any) => setSelectedFile(e.target.files[0])}
+          />
+          <button type="submit">Upload</button>
+        </form>
+
+        <div>
+          <h1>All Music</h1>
+          {allMusic &&
+            allMusic.map((music: any) => {
+              return (
+                // <div>
+                  <audio controls>
+                    <source
+                      src={music.audio_url}
+                      type={music.mimetype}
+                    ></source>
+                  </audio>
+                // </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
